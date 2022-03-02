42b86561091143532c75601efb4eb2fb605ee4de
SUANUSHA <98738330+SUANUSHA@users.noreply.github.com>
2022-03-02 13:44:43 +0530

    anu

---
+++ malloc.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    int n, i;
+    printf("Entered number of integers :");
+    scanf("%d",&n);
+     int *ptr = (int*)malloc(n * sizeof(int));
+    if (ptr == NULL)
+    {
+        printf("Memory not allocated.\n");
+        exit(0);
+    }
+    for (i = 0; i < n; ++i)
+        {
+            printf("Enter an integer:");
+            scanf("%d", ptr+i);
+        }    
+        for (i = 0; i < n; ++i) 
+            printf("%d", *(ptr+i));
+            
+     return 0;
+}
