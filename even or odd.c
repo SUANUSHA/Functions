157e09b28852e748c8155ce641d59cae73bb100c
SUANUSHA <98738330+SUANUSHA@users.noreply.github.com>
2022-03-02 14:08:40 +0530

    anu

---
+++ even or odd.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int isEven(int n)
+{
+    if(n%2==0)  
+    {
+        printf("\n%d is an even number",n);
+    }
+    else
+    {
+        printf("\n%d is an odd number",n);
+    }
+}
+int main()
+{
+    int n;
+    printf("Enter a number: ");
+    scanf("%d",&n);
+    isEven(n); 
+    return 0;
+}
